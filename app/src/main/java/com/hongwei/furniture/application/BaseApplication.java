5ffd5f9209ff20f83807e7d44b6d7be3fcb23414
Outstan <1187453132@qq.com>
2021-01-04 23:49:04 +0800

    two

---
+++ app/src/main/java/com/hongwei/furniture/application/BaseApplication.java
@@ -0,0 +1,48 @@
+package com.hongwei.furniture.application;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.hongwei.basiclib.log.LogLevel;
+import com.hongwei.basiclib.network.HttpMethods;
+
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * @author xyb
+ * @description:
+ * @date : 2020-12-31
+ */
+public class BaseApplication extends Application {
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+
+        //访问网络初始化
+        HttpMethods
+                .getInstanceBuilder()
+               // .setBaseUrl(Constants.BASE_URL)//设置域名
+                .setLogLevel(LogLevel.ERROR)//设置日志打印级别，使用默认的日志打印才需要设置这个
+                .setLogName("HttpManager")//设置默认日志打印名字
+                .setIsOpenLog(true)//设置是否开启框架默认的日志打印
+                .setUseDefaultSSLSocketFactory(true)
+//                .setCookieJar(new CookieJarImpl())//设置自定义的cookiejar
+//                .setLogger(new HttpLogger())//设置自定义logger，此设置是打印网络请求的数据（如果设置了自定义的，则框架默认的则不需要设置）
+//                .setLevel(LoggerLevel.BODY)//设置日志打印级别（自定义logger可设置，框架默认的是BODY级别，如果上架需要关闭日志打印，则设置setIsOpenLog(false)即可）
+                .setReadTimeOut(60)
+                .setConnectTimeOut(60)
+                .setWriteTimeOut(60)
+//                .setInterceptor(new CommonParametersInterceptor())//设置拦截器
+//                .setNetworkInterceptor(new CommonParametersInterceptor())//设置拦截器
+                .setFactory(GsonConverterFactory.create())//设置自定义解析器
+                .setInterceptors(new HttpLoggingInterceptor());//设置多个拦截器
+    }
+
+    public static Context getInstance(){
+        return mContext;
+    }
+}
--- basicLib/src/main/java/com/hongwei/basiclib/utils/TimeUtil.java
+++ basicLib/src/main/java/com/hongwei/basiclib/utils/TimeUtil.java
@@ -1,10 +1,117 @@
 package com.hongwei.basiclib.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * FileName: TimeUtil
  * Author: XYB
  * Date: 2020/12/29
- * Description:
+ * Description:TODO 时间格式化
  */
 public class TimeUtil {
+    /**
+     * 将时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String yyyyMMdd(long time) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date(time);
+        return format.format(date);
+    }
+
+    /***
+     * 时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String HHmmss(long time) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(time);
+        return format.format(date);
+    }
+
+    /***
+     * 时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String yyyyMMddHHmm(long time) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = new Date(time);
+        return format.format(date);
+    }
+
+    /***
+     * 时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String yyyyMMddHHmmss(long time) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(time);
+        return format.format(date);
+    }
+
+    /***
+     * 时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String MMddHHmm(long time) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd HH:mm");
+        Date date = new Date(time);
+        return format.format(date);
+    }
+
+    /***
+     * 时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String MMddHHmmss(long time) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd HH:mm:ss");
+        Date date = new Date(time);
+        return format.format(date);
+    }
+
+    /***
+     * 时间戳转换为时间
+     *
+     * @param time
+     * @return
+     */
+    public static String byPattern(long time, String pattern) {
+        if (time == 0) {
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        Date date = new Date(time);
+        return format.format(date);
+    }
 }
